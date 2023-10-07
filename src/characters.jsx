3ab520e5fe0f16d5ea61e015c769a5df2d75d04f
adrian1lara <adrianlara203@gmail.com>
2023-10-07 01:25:20 -0500

    list of objects

---
+++ src/characters.jsx
@@ -0,0 +1,61 @@
+import luffy from './assets/luffy.jpg'
+import nami from './assets/nami.jpg'
+import zoro from './assets/zoro.jpg'
+import sanji from './assets/sanji.jpg'
+import chopper from './assets/chopper.jpg'
+import ussop from './assets/ussop.jpg'
+import robin from './assets/robin.jpg'
+
+
+const characters = [
+    {
+        id: 1,
+        name: 'Luffy',
+        src: luffy,
+        clicked: false
+    },
+
+    {
+        id: 2,
+        name: 'Nami',
+        src: nami,
+        clicked: false
+    },
+
+    {
+        id: 3,
+        name: 'Zoro',
+        src: zoro,
+        clicked: false
+    },
+
+    {
+        id: 4,
+        name: 'Sanji',
+        src: sanji,
+        clicked: false
+    },
+
+    {
+        id: 5,
+        name: 'Chopper',
+        src: chopper,
+        clicked: false
+    },
+
+    {
+        id: 6,
+        name: 'Ussop',
+        src: ussop,
+        clicked: false
+    },
+
+    {
+        id: 7,
+        name: 'Robin',
+        src: robin,
+        clicked: false
+    }
+]
+
+export default characters
