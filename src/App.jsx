bc202031c765ebc3cd4d782ae8053450db10b4b9
adrian1lara <adrianlara203@gmail.com>
2023-10-09 00:42:54 -0500

    added box and header

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,22 @@
 import Game from './components/Game'
-import { Box, Heading } from '@chakra-ui/react'
-
+import { Box, Center, Heading } from '@chakra-ui/react'
 function App() {
 
   return (
-    <>
+    <Box minH={'100vh'} bg={'#0F0F0F'}>
       <header>
-        <Heading>
-          Memory Cards
+        <Center>
+        <Heading color={'white'} mt={4} as={'h1'} >
+          One Piece Memory Game
         </Heading>
+        </Center>
       </header>
       <Box
         mt={4}
-      >
+        >
         <Game />
       </Box>
-
-    </>
+    </Box>
   )
 }
 
